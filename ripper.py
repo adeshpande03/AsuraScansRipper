56d5ef904f91eab4199bee4b79c0db664b810bfa
Akhil Deshpande <deshpandeakhils@gmail.com>
2024-08-02 20:21:14 -0600

    fixing path changes

--- ripper.py
+++ ripper.py
@@ -2,20 +2,8 @@ import requests
 import shutil
 from pprint import pprint
 import re
-import os
 from PIL import Image
 import sys
-
-# def test(url):
-#     response = requests.get(url, stream=True)
-#     with open('Comic_ORV/chapter1.webp', 'wb') as out_file:
-#         shutil.copyfileobj(response.raw, out_file)
-
-
-# test("https://gg.asuracomic.net/storage/comics/105/c2c36d3d-ec83-4556-9cfc-9d0fb23b08c7/12.webp")
-
-
-from PIL import Image
 import os
 
 
@@ -37,43 +25,20 @@ def convert_and_concatenate_vertical(folder_path, output_path):
         png_path = fp.rsplit(".", 1)[0] + ".png"  # Generate PNG path
         image.save(png_path, format="PNG")
         converted_images.append(Image.open(png_path))
-
-    # Calculate total height and maximum width
     total_height = sum(img.height for img in converted_images)
     max_width = max(img.width for img in converted_images)
-
-    # Create a new image with the total height and maximum width
     new_image = Image.new("RGBA", (max_width, total_height))
-    # Paste images one below the other
     current_height = 0
     for img in converted_images:
-        # If the image width is not the maximum, resize it proportionally
         if img.width != max_width:
             img = img.resize(
                 (max_width, int(img.height * max_width / img.width)), Image.LANCZOS
             )
-        new_image.paste(
-            img,
-            (0, current_height),
-            img
-        )
-
+        new_image.paste(img, (0, current_height), img)
         current_height += img.height
-
-    # Save the concatenated image
     new_image.save(output_path)
-
-    # Remove the original and converted PNG files
     for fp in file_paths + [img.filename for img in converted_images]:
         os.remove(fp)
-        # print(f"Removed file: {fp}")
-
-
-# chapter = 75
-# convert_and_concatenate_vertical(
-#     f"/Users/akhildeshpande/Documents/VSCode/Python/AsuraScansRipper/Comic_ORV/{chapter}",
-#     f"/Users/akhildeshpande/Documents/VSCode/Python/AsuraScansRipper/Comic_ORV/{chapter}/{chapter}.png",
-# )
 
 
 def saveChapter(comic_folder_name, beg_chapter, end_chapter, comic_url_up_to_chapter):
@@ -101,19 +66,15 @@ def saveChapter(comic_folder_name, beg_chapter, end_chapter, comic_url_up_to_cha
             with open(full_path + "/" + img[-7:], "wb") as out_file:
                 shutil.copyfileobj(response.raw, out_file)
         convert_and_concatenate_vertical(
-            f"/Users/akhildeshpande/Documents/VSCode/Python/AsuraScansRipper/Comic_ORV/{chapter}",
-            f"/Users/akhildeshpande/Documents/VSCode/Python/AsuraScansRipper/Comic_ORV/{chapter}/{chapter}.png",
+            f"{full_path}",
+            f"{full_path}/{chapter}.png",
         )
         print(f"Chapter {chapter} done.")
-        # concatenate_vertical(
-        #     full_path,
-        #     full_path + "/" + str(chapter) + ".webp",
-        # )
 
 
 saveChapter(
-    "ORV",
+    "Return of the Mount Hua Sect",
     sys.argv[1],
     sys.argv[2],
-    "https://asuracomic.net/series/omniscient-readers-viewpoint-ad808d5b/chapter/",
+    "https://asuracomic.net/series/return-of-the-mount-hua-sect-e35abd30/chapter/",
 )
